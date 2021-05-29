e8f4ea14e3155df515e2b0d874ac5bd77af7aab1
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-30 01:08:13 +0900

    comment 모델 작성

---
+++ back/models/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const commentSchema = mongoose.Schema(
+  {
+    writer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    videoId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    responseTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = { Comment };
