14d60a6b72df25275f0dcb24241b98a53dfce07c
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-29 21:02:50 +0900

    Subscribe 버튼 버그 수정

--- front/src/components/Subscribe/index.tsx
+++ front/src/components/Subscribe/index.tsx
@@ -16,12 +16,7 @@ function Subscribe({ userTo }: ISubscribe) {
     };
     if (subscribed) {
       axios
-        .delete("/api/subscribe/unSubscribe", {
-          data: {
-            userTo: body.userTo,
-            userFrom: body.userFrom,
-          },
-        })
+        .delete(`/api/subscribe/unSubscribe/${body.userTo}/${body.userFrom}`)
         .then((response) => {
           if (response.data.success) {
             setSubscribeNumber((prev) => prev - 1);
@@ -30,21 +25,21 @@ function Subscribe({ userTo }: ISubscribe) {
             alert("구독 취소 하는데 실패했습니다.");
           }
         });
+    } else {
+      axios.post("/api/subscribe/subscribe", body).then((response) => {
+        if (response.data.success) {
+          setSubscribeNumber((prev) => prev + 1);
+          setSubscribed((prev) => !prev);
+        } else {
+          alert("구독 하는데 실패했습니다.");
+        }
+      });
     }
-    axios.post("/api/subscribe/subscribe").then((response) => {
-      if (response.data.success) {
-        setSubscribeNumber((prev) => prev + 1);
-        setSubscribed((prev) => !prev);
-      } else {
-        alert("구독 하는데 실패했습니다.");
-      }
-    });
   }, [subscribed, userTo]);
 
   useEffect(() => {
     axios.get(`/api/subscribe/subscribeNumber/${userTo}`).then((response) => {
       if (response.data.success) {
-        console.log(response.data);
         setSubscribeNumber((prev) => response.data.subscribeNumber);
       } else {
         alert("구독자 수를 불러오지 못했습니다.");
