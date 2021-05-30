41b57ee42379c11e1755f7e1cb3326c09293823f
GYUHA_HWANG <gyuha0310@gmail.com>
2021-05-30 21:44:39 +0900

    comment 라우터 추가

--- back/routes/comment.js
+++ back/routes/comment.js
@@ -3,9 +3,25 @@ const router = express.Router();
 const { Comment } = require("../models/Comment");
 
 router.get("/getComments/:videoId", (req, res) => {
-  Comment.find({ videoId: req.params.videoId }).exec((err, comments) => {
-    if (err) return res.status(400).send(err);
-    res.status(200).json({ success: true, comments });
+  Comment.find({ videoId: req.params.videoId })
+    .populate("writer", "isAuth isAdmin name email lastname role image")
+    .exec((err, comments) => {
+      if (err) return res.status(400).json({ success: false, err });
+      res.status(200).json({ success: true, comments });
+    });
+});
+
+router.post("/saveComment", (req, res) => {
+  const comment = new Comment(req.body);
+
+  comment.save((err, comment) => {
+    if (err) return res.status(400).json({ success: false, err });
+    Comment.find({ _id: comment._id })
+      .populate("writer", "isAuth isAdmin name email lastname role image")
+      .exec((err, comment) => {
+        if (err) return res.status(400).json({ success: false, err });
+        res.status(200).json({ success: true, comment });
+      });
   });
 });
 
